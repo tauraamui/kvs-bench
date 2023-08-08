01993768f6bf9401fcfd6f319f38f0387db426b3
tauraamui <adampstringer@protonmail.com>
2023-08-08 10:26:39 +0100

    bench: define test for SQLite loading 500 records

--- benchmark_load_sl_test.go
+++ benchmark_load_sl_test.go
@@ -40,3 +40,33 @@ func BenchmarkSLLoad(b *testing.B) {
 	}
 	b.StopTimer()
 }
+
+func BenchmarkSLLoad500Records(b *testing.B) {
+	defer func() {
+		os.RemoveAll("./data")
+	}()
+	os.Mkdir("./data", fs.ModePerm)
+	db, err := gorm.Open(sqlite.Open("./data/data.db"), &gorm.Config{})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	rootUUID := uuid.New()
+
+	db.AutoMigrate(&SQLBalloon{})
+
+	for i := 0; i < 500; i++ {
+		color := "RED"
+		if i%2 == 0 {
+			color = "BLUE"
+		}
+		db.Create(&SQLBalloon{UUID: rootUUID, Balloon: Balloon{Color: color, Size: i}})
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		balloons := []SQLBalloon{}
+		db.Where("uuid = ?", rootUUID).Find(&balloons)
+	}
+	b.StopTimer()
+}
