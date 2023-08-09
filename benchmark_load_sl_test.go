9003bc5c27b9f5e21f16bf6988e08641c0d5ec4f
tauraamui <adampstringer@protonmail.com>
2023-08-09 07:08:14 +0100

    bench: define new benchmarks for SQLite

--- benchmark_load_sl_test.go
+++ benchmark_load_sl_test.go
@@ -70,3 +70,93 @@ func BenchmarkSLLoad500Records(b *testing.B) {
 	}
 	b.StopTimer()
 }
+
+func BenchmarkSLLoad100RecordsQueryColour(b *testing.B) {
+	defer func() {
+		os.RemoveAll("./data")
+	}()
+	os.Mkdir("./data", fs.ModePerm)
+	db, err := gorm.Open(sqlite.Open("./data/data.db"), &gorm.Config{})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	rootUUID := uuid.New()
+
+	db.AutoMigrate(&SQLBalloon{})
+
+	for i := 0; i < 100; i++ {
+		color := "RED"
+		if i%2 == 0 {
+			color = "BLUE"
+		}
+		db.Create(&SQLBalloon{UUID: rootUUID, Balloon: Balloon{Color: color, Size: i}})
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		balloons := []SQLBalloon{}
+		db.Where("uuid = ? AND color = ?", rootUUID, "RED").Find(&balloons)
+	}
+	b.StopTimer()
+}
+
+func BenchmarkSLLoad500RecordsQuerySizeNoMatches(b *testing.B) {
+	defer func() {
+		os.RemoveAll("./data")
+	}()
+	os.Mkdir("./data", fs.ModePerm)
+	db, err := gorm.Open(sqlite.Open("./data/data.db"), &gorm.Config{})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	rootUUID := uuid.New()
+
+	db.AutoMigrate(&SQLBalloon{})
+
+	for i := 0; i < 500; i++ {
+		color := "RED"
+		if i%2 == 0 {
+			color = "BLUE"
+		}
+		db.Create(&SQLBalloon{UUID: rootUUID, Balloon: Balloon{Color: color, Size: i}})
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		balloons := []SQLBalloon{}
+		db.Where("uuid = ? AND size = ?", rootUUID, i).Find(&balloons)
+	}
+	b.StopTimer()
+}
+
+func BenchmarkSLLoad500RecordsQueryColour(b *testing.B) {
+	defer func() {
+		os.RemoveAll("./data")
+	}()
+	os.Mkdir("./data", fs.ModePerm)
+	db, err := gorm.Open(sqlite.Open("./data/data.db"), &gorm.Config{})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	rootUUID := uuid.New()
+
+	db.AutoMigrate(&SQLBalloon{})
+
+	for i := 0; i < 100; i++ {
+		color := "RED"
+		if i%2 == 0 {
+			color = "BLUE"
+		}
+		db.Create(&SQLBalloon{UUID: rootUUID, Balloon: Balloon{Color: color, Size: i}})
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		balloons := []SQLBalloon{}
+		db.Where("uuid = ? AND color = ?", rootUUID, "RED").Find(&balloons)
+	}
+	b.StopTimer()
+}
